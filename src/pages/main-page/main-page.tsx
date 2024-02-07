738b98dad16677b418f2dd6d6668222593ee9a58
Ratiborz <konisgung@gmail.com>
2024-02-07 16:43:17 +0300

    first commit

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
-import reactLogo from '/react.svg';
-import viteLogo from '/vite.svg';
-import tsLogo from '/ts.svg';
 import './main-page.css';
+import reactLogo from '/react.svg';
+import tsLogo from '/ts.svg';
+import viteLogo from '/vite.svg';
 
 export const MainPage: React.FC = () => {
     const [count, setCount] = useState(0);
