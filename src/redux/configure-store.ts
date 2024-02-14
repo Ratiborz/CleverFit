efba76b887f9016d7916c354bd22b3de026ed0bb
Ratiborz <konisgung@gmail.com>
2024-02-14 21:49:51 +0300

    feat: add historyRouter and routes

--- src/redux/configure-store.ts
+++ src/redux/configure-store.ts
@@ -1,8 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { createBrowserHistory } from 'history';
+import { createReduxHistoryContext } from 'redux-first-history';
+
+const { createReduxHistory, routerMiddleware, routerReducer } = createReduxHistoryContext({
+    history: createBrowserHistory(),
+});
 
 export const store = configureStore({
-    reducer: {},
+    reducer: combineReducers({
+        router: routerReducer,
+    }),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(routerMiddleware),
 });
+
+export const history = createReduxHistory(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
